62dfa8c6e7c4fbca070116ca3948c383880a03ca
sami <samiahmed0f0@gmail.com>
2022-05-15 14:38:32 +0600

    updated meta content example

--- test/inc/header.php
+++ test/inc/header.php
@@ -2,7 +2,27 @@
 <html lang="en">
 <head>
    <meta>
+      <?php
+         $route = basename($_SERVER['PHP_SELF']);
+         switch ($route) {
+            case 'index.php':
+               $title = 'Home';
+               break;
+            case 'about.php':
+                  $title = 'About';
+               break;
+            case 'privacy.php':
+               $title = 'Privacy';
+               break;
+            default:
+            $title = 'Home';
+            break;
+         }
       
+   ?>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title><?php echo $title; ?></title>
    </meta>
    <script src="https://cdn.alkhella.com/elevatorjs/src/elevator.js"></script>
    
